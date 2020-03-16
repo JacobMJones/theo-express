efff4582dfa05689fe729aab5dda6458bb15ee3b
jacobmjones <mrjacobtaiwan@gmail.com>
2020-03-15 23:49:19 -0400

    test

--- express/server.js
+++ express/server.js
@@ -33,7 +33,7 @@ async function getSpreadsheetData() {
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-//app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);

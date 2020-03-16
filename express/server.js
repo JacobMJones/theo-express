cd48e2ba70193afbec381bc87e806e221b882964
jacobmjones <mrjacobtaiwan@gmail.com>
2020-03-15 23:09:16 -0400

    test as api

--- express/server.js
+++ express/server.js
@@ -5,14 +5,32 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+const Tabletop = require('tabletop');
+const spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1s04Abd0bfNFVYOU-_8pnsTqq1N8mbYDpT5iYnzYlPas/edit?usp=sharing'
+
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+
+app.get("/", async (req, res, next) => {
+  console.log('1')
+  const data = await getSpreadsheetData()
+
+
+  res.json({ data: data })
+
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+async function getSpreadsheetData() {
+  const d = await Tabletop.init({
+    key: spreadsheetURL,
+    simpleSheet: true
+  }
+  ).then(function (data, tabletop) {
+
+    return data
+  })
+  return d
+}
+
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
